781f195405ee34ca80846f9c146aa012eb320e9f
JosephMarlon <111172526+JosephMarlon@users.noreply.github.com>
2025-01-26 14:55:32 +0200

    removed the message var
    I had written my code using a let message var, which did not work, so i added up just using the console.log

--- index.js
+++ index.js
@@ -5,19 +5,17 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-let drinkType = userPreference || 'Regular';
 
-console.log(`The user prefers a ${drinkType} Monster Energy drink.`);
-
+let preference = userPreference || 'Regular';
+console.log(`User prefers: ${preference}`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-// Use a ternary operator to decide the message
-let message = cansLeft < 5 ? "Time to restock!" : "We're stocked!";
-console.log(message);
+
+console.log(cansLeft < 5 ? "Time to restock!" : "We're stocked!");
 
 
 // #### Challenge 3: Workout Intensity
@@ -25,9 +23,8 @@ console.log(message);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-// Use a ternary operator to determine if a boost is needed
-let message = heartRate < 100 ? "Boost needed!" : "Energy levels are high!";
-console.log(message);
+
+console.log(heartRate < 100 ? "Boost needed!" : "Energy levels are high!");
 
 
 // #### Challenge 4: Temperature Suitability
@@ -35,9 +32,7 @@ console.log(message);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-// Use a ternary operator to determine if the drink is chilled properly
-let message = currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!";
-console.log(message);
+console.log(currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!");
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -53,10 +48,7 @@ let currentHour = 22;
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
 // Use a ternary operator to determine if it's a good idea to have a Monster Energy drink
-let message = currentHour >= 7 && currentHour < 24 
-  ? "Unleash the beast!" 
-  : "Better stick to water.";
-  console.log(message);
+console.log(currentHour >= 7 && currentHour <= 23 ? "Unleash the beast!" : "Better stick to water.");
 
 
 
